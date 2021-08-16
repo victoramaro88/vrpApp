695da09eed728ea889d97c03345f7e6cd953fccb
Victor Amaro <amaro.victor@hotmail.com>
2021-08-16 19:00:58 -0300

    .

--- src/app/pages/configs/parametros-vrp/parametros-vrp.component.ts
+++ src/app/pages/configs/parametros-vrp/parametros-vrp.component.ts
@@ -4,6 +4,7 @@ import {MessageService} from 'primeng/api';
 import { VRPModel } from 'src/app/models/vrp.model';
 import { ParametrosVRPModel } from 'src/app/models/parametrosVRP.model';
 import { SelectItem } from 'primeng/api';
+import { _isNumberValue } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-parametros-vrp',
@@ -29,6 +30,7 @@ export class ParametrosVRPComponent implements OnInit {
   clonedItens: { [s: string]: ParametrosVRPModel; } = {};
   item2: ParametrosVRPModel[] = [];
   novoparametro = false;
+  idVRP = 0;
 
   pressaoInvalida = false;
   horaInInvalida = false;
@@ -65,6 +67,7 @@ export class ParametrosVRPComponent implements OnInit {
   }
 
   EditarParametros(idVRP: number) {
+    this.idVRP = idVRP;
     this.boolLoading = true;
     // let indexListaVRP = this.listaVRP.findIndex(v => v.idVRP === idVRP);
     // this.objVRP = this.listaVRP[indexListaVRP];
@@ -147,14 +150,24 @@ export class ParametrosVRPComponent implements OnInit {
     }
 
     if(this.VerificaHoraValida(this.objParametro.horaFinal)) {
-      if(this.objParametro.horaInicial.split(':')[0] <= this.objParametro.horaFinal.split(':')[0]) {
-        if(this.objParametro.horaInicial.split(':')[1] < this.objParametro.horaFinal.split(':')[1]) {
-          this.horaFiInvalida = false;
-        } else {
+      if(this.objParametro.horaInicial.split(':')[0] === this.objParametro.horaFinal.split(':')[0]) {
+        if(this.objParametro.horaInicial.split(':')[1] > this.objParametro.horaFinal.split(':')[1]) {
+          console.log('Horário final não pode ser menor que inicial.');
           this.horaInInvalida = true;
           this.horaFiInvalida = true;
+        } else if(this.objParametro.horaInicial.split(':')[1] === this.objParametro.horaFinal.split(':')[1]) {
+          console.log('Horários não podem ser iguais.');
+          this.horaFiInvalida = true;
         }
-      } else {
+        else {
+          this.horaInInvalida = false;
+          this.horaFiInvalida = false;
+        }
+      } else if(this.objParametro.horaInicial.split(':')[0] < this.objParametro.horaFinal.split(':')[0]) {
+        this.horaInInvalida = false;
+        this.horaFiInvalida = false;
+      }
+      else {
         this.horaInInvalida = true;
         this.horaFiInvalida = true;
       }
@@ -164,10 +177,10 @@ export class ParametrosVRPComponent implements OnInit {
   }
 
   VerificaHoraValida(horaMin: string) {
-    console.log(horaMin.split(':'));
     if(horaMin.length === 5 && horaMin.split(':').length <= 2) {
       let hora = horaMin.split(':')[0];
       let mint = horaMin.split(':')[1];
+
       if(+hora <= 23 && +mint <= 59) {
         return true;
       } else {
@@ -175,6 +188,63 @@ export class ParametrosVRPComponent implements OnInit {
       }
     }else {
       return false;
+    }
+  }
+
+  SalvarNovoParametro() {
+    this.boolLoading = true;
+    if(!this.pressaoInvalida && !this.horaInInvalida  && !this.horaFiInvalida
+      && this.objParametro.pressao >= 0 && this.objParametro.horaFinal.length > 0 && this.objParametro.horaInicial.length > 0) {
+
+      let horaIn = this.objParametro.horaInicial.split(':')[0];
+      let minutiIn = this.objParametro.horaInicial.split(':')[1];
+      let horaFin = this.objParametro.horaFinal.split(':')[0];
+      let minutiFin = this.objParametro.horaFinal.split(':')[1];
+      const horaInicial = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),
+      (horaIn ? +horaIn : 0), minutiIn ? +minutiIn : 0);
+      const horaFinal = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),
+      (horaFin ? +horaFin : 0), minutiFin ? +minutiFin : 0);
+      console.log(horaInicial);
+      console.log(horaFinal);
+
+      for (const itemVRP of this.listaParametrosVRP) {
+        let horaInVRP = itemVRP.horaInicial.split(':')[0];
+        let minutiInVRP = itemVRP.horaInicial.split(':')[1];
+        let horaFinVRP = itemVRP.horaFinal.split(':')[0];
+        let minutiFinVRP = itemVRP.horaFinal.split(':')[1];
+        const horaInicialVRP = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),
+        (horaInVRP ? +horaInVRP : 0), minutiInVRP ? +minutiInVRP : 0);
+        const horaFinalVRP = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),
+        (horaFinVRP ? +horaFinVRP : 0), minutiFinVRP ? +minutiFinVRP : 0);
+
+        if(horaIn >= horaInVRP && horaFin <= horaFinVRP) {
+          console.log('JÁ EXISTE PROGRAMAÇÃO PARA ESSE HORÁRIO');
+        }
+
+
+        console.log(horaInicialVRP);
+        console.log(horaFinalVRP);
+
+        console.log('Diff hora inicial: ' + (horaInicial.getTime()-horaInicialVRP.getTime())/ (1000 * 60));
+        console.log('Diff hora inicial: ' + (horaInicialVRP.getTime()-horaInicial.getTime())/ (1000 * 60));
+
+
+        // const a = new Date(2021, 0, 1);
+        // const b = new Date(2021, 0, 5);
+        // console.log((b.getTime() - a.getTime()) / (1000 * 60 * 60 * 24));
+        // console.log(itemVRP);
+
+
+      }
+
+      this.objParametro.flStatus = true;
+      this.objParametro.idVRP = this.idVRP;
+
+      this.boolLoading = false;
+      console.log(this.objParametro);
+    } else {
+      this.boolLoading = false;
+      this.messageService.add({severity:'warn', summary:'Atenção', detail:'Valores inválidos, verifique.'});
     }
   }
 }
